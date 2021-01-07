5655cff7fa1553fba46304f107b9aff2f5d7c96c
An-Chi Pham <an-chi.pham@edu.devinci.fr>
2021-01-07 19:24:27 +0100

    ajout main.c question 5.5

---
+++ main.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+int main(int argc,char *argv[]){
+	if(argc>2){
+		argc=atoi(argv[1]);
+		for(int i=1; i<=argc; i++){
+			printf("%d",i);
+		}
+		return 0;
+	}
+	else if(argc==2){
+		argc=atoi(argv[1]);int i=1;
+		if(isalpha(argv[1][0])!=0||argc==0){
+			return 0;
+		}
+		do{
+			printf("%d\n",i);
+			i++;
+		}
+		while(i<=argc);
+		return 0;
+	}
+	else if(argc==1){
+		for(int i=1; i<=10; i++){
+			printf("%d\n",i);
+		}
+		return 0;
+	}
+}
+
+
